fc98eeef685b2f3233a9b9131185a9c3f3052f18
serifeecinar@outlook.com <serifeecinar@outlook.com>
2023-03-28 10:29:06 +0300

    ++

--- Data Acces/Repositories/SertifikaRepository.cs
+++ Data Acces/Repositories/SertifikaRepository.cs
@@ -1,11 +1,8 @@
 ﻿using Data_Acces.Models;
 using DataAcces.Interfaces;
-using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataAcces.Repositories
 {
